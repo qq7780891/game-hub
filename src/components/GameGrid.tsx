30e002aeadf86018fcf54faf837fbf7041a089fc
Your Name <375510180@e.com>
2023-04-06 16:44:00 +0800

    静态数据和网站背景色

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -11,9 +11,8 @@ interface Props {
 function GameGrid({gameQuery}:Props) {
     const {data,error,isLoading} = useGames(gameQuery);
     const skeletons=[1,2,3,4,5,6];
+    if (error) return <Text>{error}</Text>;
     return (
-        <>
-        {error && <Text>{error}</Text>}
             <SimpleGrid columns={{sm:1,md:2,lg:3,xl:4}} padding={'10px'} spacing={6}>
                 {isLoading&&skeletons.map(skeleton=>
                    <GameCardContainer key={skeleton}>
@@ -26,7 +25,6 @@ function GameGrid({gameQuery}:Props) {
                     </GameCardContainer>
                 )}
             </SimpleGrid>
-        </>
     );
 }
 
